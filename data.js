33e578b4afc98c14e989aea01801cd8ff3d05b7a
unknown <nyameba2018@gmail.com>
2023-09-20 03:59:18 -0700

    product setup

---
+++ data.js
@@ -0,0 +1,33 @@
+// const data = [
+//     {
+//         id:1,
+//         name:"tomato",
+//         image:"image/Tomatoes.JPG",
+//         desc:"Fresh organic Tomatoes",
+//         price:10.00
+//     },
+
+//     {
+//         id:2,
+//         name:"pepper",
+//         image:"image/Peppers.JPG",
+//         desc:"Fresh organic Peppers",
+//         price:6.00
+//     },
+
+//     {
+//         id:3,
+//         name:"guava",
+//         image:"image/Field Blur.jpg",
+//         desc:"Fresh organic Gauva",
+//         price:15.00
+//     },
+
+//     {
+//         id:4,
+//         name:"Pizza",
+//         image:"image/0_GgFLcULUNxQp94FM.jpg",
+//         desc:"Tasty pizza",
+//         price:10.00
+//     },
+// ]
